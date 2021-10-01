52d42b2d4edcfe2cddb85afefc0454f23fb01c57
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2021-09-30 17:02:52 -0700

    main.py 02

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def stateChange(input, state, map, p_e):
+def stateChange(inp, state, matMap, p_e):
   
   return newState
+
+def genMap(size = (5,5), shops = [], obstacles = [], negRewards = []):
+  matMap = [[0 for i in range(size[1])] for j in range(size[0])]
+  for shop in shops:
+    matMap[shop[0]][shop[1]] = 
 
 if __name__ == "__main__":
   
