002a11a1fd2019aa59f79a1fb745d9ff75a67dfb
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2021-09-30 17:29:05 -0700

    main.py 04

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import numpy as np
+import random
+import copy
 from matplotlib import pyplot as plt
 
-def stateChange(inp, state, matMap, p_e):
+actions   = [(1,0),(-1,0),(0,1),(0,-1),(0,0)]
+shops     = [(2,0), (2,2)]
+obstacles = [(1,1), (1,3),(2,1),(2, 3)]
+
+def stateChange(action, state, matMap, p_e):
+  newState = state.copy()
+  realAction = random.choices([], weights = [10, 1, 1], k = 14))
+  
+  newState[0] += realAction[0]
+  newState[1] += realAction[1]
   
   return newState
 
-def genMap(size = (5,5), shops = [], obstacles = [], negRewards = []):
+def genMap(size = (5,5), shops = [], obstacles = [], negRewPos = [], negRewVal = []):
   matMap = [[0 for i in range(size[1])] for j in range(size[0])]
   for shop in shops:
-    matMap[shop[0]][shop[1]] = 
+    matMap[shop[0]][shop[1]] = float('inf')
+  for obstacle in obstacles:
+    matMap[shop[0]][shop[1]] = float('inf')
+    
 
 if __name__ == "__main__":
   
