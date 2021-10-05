734baf50d01396b6e7b2e4f2ec93e78efc9c3ad4
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2021-10-05 16:48:49 -0700

    Undo unwanted merge

--- main.py
+++ main.py
@@ -14,36 +14,3 @@ observation = gw.get_observation(initial_state)
 sim = simulator.Simulator(gw, initial_state)
 sim.run(steps=100, render=True)
 exit()
-
-
-
-"""
-import numpy as np
-import random
-import copy
-from matplotlib import pyplot as plt
-
-actions   = [(1,0),(-1,0),(0,1),(0,-1),(0,0)]
-shops     = [(2,0), (2,2)]
-obstacles = [(1,1), (1,3),(2,1),(2, 3)]
-
-def stateChange(action, state, matMap, p_e):
-  newState = state.copy()
-  realAction = random.choices([], weights = [10, 1, 1], k = 14))
-  
-  newState[0] += realAction[0]
-  newState[1] += realAction[1]
-  
-  return newState
-
-def genMap(size = (5,5), shops = [], obstacles = [], negRewPos = [], negRewVal = []):
-  matMap = [[0 for i in range(size[1])] for j in range(size[0])]
-  for shop in shops:
-    matMap[shop[0]][shop[1]] = float('inf')
-  for obstacle in obstacles:
-    matMap[shop[0]][shop[1]] = float('inf')
-    
-
-if __name__ == "__main__":
-  
-"""
