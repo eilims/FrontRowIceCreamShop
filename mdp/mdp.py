ba719437bbe3e8fd6455ce9655facb9f8a04cbbe
Patrick Han <47380073+patrick-han@users.noreply.github.com>
2021-10-06 12:16:33 -0700

    Wrote value iteration
    - Moved self.V to MDP class
    - Moved self.policy to MDP class
    - Wrote run() for ValueIteration class
    - Wrote bellmanBackUp() for MDP class
    
    Notes:
    The current implementation of the bellman backup is really slow and can definitely be vectorized. It's also dependent on how P and R are represented (I'm also just assuming S and A are lists of states and actions)

--- mdp/mdp.py
+++ mdp/mdp.py
@@ -1,7 +1,6 @@
 # Defines classes for a general MDP problem and associated problem solvers.
 # By: Patrick Han
 
-
 class MDP:
     def __init__(self, states, actions, probabilities, rewards, horizon, gamma, epsilon):
         """
@@ -24,6 +23,38 @@ class MDP:
         self._gamma = gamma
         self._epsilon = epsilon
 
+        # Initialize a deterministic policy which is a mapping between states and actions
+        self.policy = {}
+        for state in self._states:
+            # We can check to make sure that every state has an action later (i.e. not False), this is a really hacky way to do it for now
+            self.policy[state] = False
+
+        # Value Iteration
+        self.V = np.zeros(len(self._states)) # Zero initialize values
+
+
+    def bellmanBackup(self):
+        """
+        Applies the bellman operator T on the current value V and updates self.V and self.policy
+        """
+        # TODO: Highly dependent on the representations of P and R!!
+
+        P = self._probabilities
+        R = self._rewards
+
+        # Very slow way of doing things, should be able to be vectorized but right now my brain hurts after writing this class all morning
+        for i, s in enumerate(self.V):
+            Q = np.zeros(len(self._actions))
+            for k, a in enumerate(self._actions):
+                running_sum = 0 # Sum over all s_primes (future state)
+                for j, s_prime in enumerate(self._states):
+                    running_sum += P[a][i][j] * (R[a][i][j] + self._gamma * self.V[i])
+                Q[k] = running_sum
+            
+            self.V[i] = np.max(Q) # Maximize over all actions to get new V
+            self.policy[s] = self._actions(np.argmax(Q)) # Argmax over all actions to update the policy
+            
+
 
 class ValueIteration(MDP):
     def __init__(self, states, actions, probabilities, rewards, horizon, gamma, epsilon):
@@ -40,22 +71,12 @@ class ValueIteration(MDP):
         """
         MDP.__init__(self, states, actions, probabilities, rewards, horizon, gamma, epsilon)
 
-        # Initialize the deterministic policy which is a mapping between states and actions
-        # Therefore, this dictionary should have each state as a key, each with 1 action as the value
-        self.policy = {}
-        for state in states:
-            # We can check to make sure that every state has an action later (i.e. not False), this is a really hacky way to do it for now
-            self.policy[state] = False 
 
         # Decide if we need to solve on an infinite or finite horizon
         self.use_infinite_horizon = False
         if self._horizon == -1:
             self.infinite_horizon = True
         self.iteration = 0 # If horizon if finite, we need to check 
-
-
-        # Value Iteration variables
-        # TODO: self.V = np.zeros("sizeof"(S)) # Zero initialize values, this is just pseudocode for now since I don't know how S is represented
 
 
     def run(self):
@@ -68,15 +89,13 @@ class ValueIteration(MDP):
 
             Vprevious = self.V.copy()
 
-
-            # Apply bellman operator to current V to calculate the new V
-            # TODO: Highly dependent on the representations of P and R
+            # Apply bellman operator to update V and the policy
+            self.bellmanBackup()
 
             # Stopping criteria
             if self.use_infinite_horizon:
-                # TODO:~ if np.max(np.abs(Vprevious - self.V)) <= self._epsilon: # If the max absolute difference is less than epsilon...
-                #           break
-                # Also need to bound this or something so the loop doesn't run forever....
+                if np.max(np.abs(Vprevious - self.V)) <= self._epsilon: # If the max absolute difference is less than epsilon
+                    break
             else if self.iteration == self._horizon:
                 break
 
