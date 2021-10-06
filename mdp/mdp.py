29db0639e06edcc51ac058e2c3d054c1626619e5
Patrick Han <47380073+patrick-han@users.noreply.github.com>
2021-10-06 14:32:30 -0700

    Implement PolicyIteration
    - Implement policy iteration
    - Bellman operator should only update Value during Value iteration, added update_value argument for it
    - Moved policy init into VI and PI subclasses

--- mdp/mdp.py
+++ mdp/mdp.py
@@ -1,5 +1,6 @@
 # Defines classes for a general MDP problem and associated problem solvers.
 # By: Patrick Han
+import random
 
 class MDP:
     def __init__(self, states, actions, probabilities, rewards, horizon, gamma, epsilon):
@@ -23,17 +24,14 @@ class MDP:
         self._gamma = gamma
         self._epsilon = epsilon
 
-        # Initialize a deterministic policy which is a mapping between states and actions
+        # Policy maps between states and actions
         self.policy = {}
-        for state in self._states:
-            # We can check to make sure that every state has an action later (i.e. not False), this is a really hacky way to do it for now
-            self.policy[state] = False
 
-        # Value Iteration
+        # Value initalization
         self.V = np.zeros(len(self._states)) # Zero initialize values
 
 
-    def bellmanBackup(self):
+    def bellmanBackup(self, update_value):
         """
         Applies the bellman operator T on the current value V and updates self.V and self.policy
         """
@@ -51,7 +49,8 @@ class MDP:
                     running_sum += P[a][i][j] * (R[a][i][j] + self._gamma * self.V[i])
                 Q[k] = running_sum
             
-            self.V[i] = np.max(Q) # Maximize over all actions to get new V
+            if update_value: # Value should only be updated during Value Iteration
+                self.V[i] = np.max(Q) # Maximize over all actions to get new V
             self.policy[s] = self._actions(np.argmax(Q)) # Argmax over all actions to update the policy
             
 
@@ -78,6 +77,10 @@ class ValueIteration(MDP):
             self.infinite_horizon = True
         self.iteration = 0 # If horizon if finite, we need to check 
 
+        # Initialize a deterministic policy which is a mapping between states and actions
+        for state in self._states:
+            # We can check to make sure that every state has an action later (i.e. not False), this is a really hacky way to do it for now
+            self.policy[state] = False
 
     def run(self):
         """
@@ -90,7 +93,7 @@ class ValueIteration(MDP):
             Vprevious = self.V.copy()
 
             # Apply bellman operator to update V and the policy
-            self.bellmanBackup()
+            self.bellmanBackup(update_value = True)
 
             # Stopping criteria
             if self.use_infinite_horizon:
@@ -100,4 +103,61 @@ class ValueIteration(MDP):
                 break
 
 
-# TODO: Write Policy Iteration class
+
+class PolicyIteration(MDP):
+    def __init__(self, states, actions, probabilities, rewards, horizon, gamma, epsilon):
+        """
+        Initialize a Policy Iteration MDP problem solver, inherits from MDP class
+        args:
+            states: All possible states
+            actions: Possible actions that can be taken
+            probabilities: Transition probabilities between states under some action
+            rewards: Immediate rewards received for state transitions under some action
+            horizon: Horizon, -1 for infinite or a positive integer for finite horizon
+            gamma: Discount factor on future rewards, float [0.0, 1.0]
+            epsilon: Stopping criteria, maximum change in value function for each iteration before stopping
+        """
+        MDP.__init__(self, states, actions, probabilities, rewards, horizon, gamma, epsilon)
+
+        # Initialize a randomized deterministic policy which is a mapping between states and actions
+        for state in self._states:
+            self.policy[state] = random.choice(self._actions)
+
+
+    def run(self):
+        """
+        Runs the Policy Iteration algorithm
+        """
+
+        while(True):
+            self.iteration += 1
+
+            Vprevious = self.V.copy()
+            policyPrevious = self.policy.copy()
+
+
+            # 1. Policy Evaluation: Compute V^(pi_i) from policy_i
+            num_states = len(self._states)
+            P_bar = np.array((num_states, num_states))
+            for m, s in enumerate(self._states): # Build P_bar, i.e. transition probabilities under the current policy
+                for n, s_prime in enumerate(self._states):
+                    P_bar[m][n] = self._probabilities[self.policy[s]][m][n] # The probability of going from s to s_prime under the current policy
+            
+            R_bar = np.array((num_states, num_states))
+            for m, s in enumerate(self._states): # Build R_bar, i.e. rewards under the current policy
+                for n, s_prime in enumerate(self._states):
+                    R_bar[m][n] = self._rewards[self.policy[s]][m][n] # The probability of going from s to s_prime under the current policy
+
+            D_bar = np.diag(np.matmul(P_bar, R_bar.T))
+            V_bar = np.matmul(np.linalg.inv(np.eye(num_states) - self._gamma * P_bar), D_bar)
+
+
+            # 2. Policy Refinement: Compute policy_(i+1) from V^(pi_i)
+            self.bellmanBackup(update_value = False) # Only update self.policy
+
+
+            # Stopping criteria - Either the value doesn't change between steps or policy doesn't change
+            if np.max(np.abs(Vprevious - self.V)) <= self._epsilon: # If the value is unchanging
+                break
+            if self.policy == policyPrevious: # Dunno if this is a good idea
+                break
