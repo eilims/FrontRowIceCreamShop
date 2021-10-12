d7333be21e1e8fccd8e6a9e73519581a25ba4059
DanielB <dbenusovich@gmail.com>
2021-10-11 23:11:50 -0700

    Add code to run value iteration to main. Just for testing!

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ import sys
 import os
 path = "{}/simulator".format(os.getcwd())
 sys.path.append(path)
+path = "{}/mdp".format(os.getcwd())
+sys.path.append(path)
 
 from simulator import simulator
 from gridworld import GridWorld
+from mdp import ValueIteration
 import numpy as np
 
 initial_state = np.array([0,0])
 gw = GridWorld()
+gw.init_transition_probabilites()
+gw.init_reward()
+
+test_states = list(range(len(gw._states)))
+test_actions = list(range(len(gw._actions)))
+solver = ValueIteration(test_states, test_actions, gw._transition_probabilities, gw._rewards, 20, 0.5, 1)
+solver.run()
+print(solver.policy)
 
 observation = gw.get_observation(initial_state)
 sim = simulator.Simulator(gw, initial_state)
-sim.run(steps=100, render=True)
+sim.run(steps=10, render=True)
 exit()
