c162bea9aea69551b5fea14a780b8fd0542b29d5
vishnu <devarakonda.vishnu5@gmail.com>
2021-10-19 16:51:58 -0700

    search base

---
+++ graph/search.py
@@ -0,0 +1,11 @@
+# Defines the base class for search algorithm
+# By: Vishnu Devarakonda
+import Graph from Graph
+
+
+class Search:
+    def __init__(self, graph: Graph) -> None:
+        self._graph = Graph
+
+    def run(self):
+        raise NotImplementedError
