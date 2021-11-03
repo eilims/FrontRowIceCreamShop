e9d5474971c8eb339a79f476ee4d3eba3ea34519
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2021-11-02 17:12:01 -0700

    Updated bayes.py

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -1,3 +1,29 @@
 #Implements the Bayes Filter
 #By: Aaron John Sabu
 
+numIters = 5
+
+def belpls(P_o, belMin):
+  belPls = [0 for s in range(len(all_states))]
+  sumBel = 0.0
+  for new_state in all_states:
+    belPls[state] = P_o[observation][state]*belMin[state]
+    sumBel += belPls[state]
+  belPls = [belVal/sumBel for belVal in belPls]
+  return belPls
+
+def belmin(P_t, belPls):
+  belMin = [0 for s in range(len(all_states))]
+  for new_state in all_states:
+    for state in all_states:
+      belMin[new_state] += P_t[new_state][state][action]*belPls[state][observation]
+  return belMin
+
+def state_estimation(initial_state):
+  bel0 = [0 for i in range(len(all_states))]
+  bel0[initial_state] = 1
+  belMin = belmin(P_t, bel0)
+  belPls = belpls(P_t, belMin)
+  for i in range(numIters - 1):
+    belMin = belmin(P_t, belPls)
+    belPls = belpls(P_t, belMin)
